64540d3729393d56e9bb53a39b4940fe227eb96f
Poliana Correia <poliana.karenn05@gmail.com>
2021-11-10 15:21:52 -0300

    condicionais e controle de fluxo

---
+++ JS/throwTryCatch.js
@@ -0,0 +1,19 @@
+//throw
+
+function sayMyName(name = '') {
+    if (name === '') {
+        throw 'Nome é obrigatório'
+    }
+
+    console.log('depois do erro')
+}
+
+// Try ... catch
+try {
+    sayMyName()
+
+} catch(e) {
+    console.log(e)
+}
+
+console.log('após a função de erro')
